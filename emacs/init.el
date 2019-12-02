bda084b7f5e1b70dedd1ea78fcd5739f28d82e28
sdual <shr.nrht.0623@gmail.com>
2019-12-03 00:46:44 +0900

    add thema, line number, elpa package management settings

--- emacs/init.el
+++ emacs/init.el
@@ -1,18 +1,42 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; elpa
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(fset 'package-desc-vers 'package--ac-desc-version)
+(package-initialize)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; theme
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+(load-theme 'smyx t)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; key bind settings
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Auto complete: select a candidate by using C-p or C-n key.
-(define-key ac-complete-mode-map "\C-n" 'ac-next)
-(define-key ac-complete-mode-map "\C-p" 'ac-previous)
+;; (define-key ac-complete-mode-map "\C-n" 'ac-next)
+;; (define-key ac-complete-mode-map "\C-p" 'ac-previous)
 
 ;; backspace key: crl-h
 (global-set-key "\C-h" 'delete-backward-char)
 
+;; window switching.
+(global-set-key (kbd "C-c b")  'windmove-left)
+(global-set-key (kbd "C-c n")  'windmove-down)
+(global-set-key (kbd "C-c p")    'windmove-up)
+(global-set-key (kbd "C-c f") 'windmove-right)
+
+
+;; scroll settings.
 (setq
- ;; ホイールでスクロールする行数を設定
+ ;; The number of lines to be scrolled with mouse wheel.
  mouse-wheel-scroll-amount '(1 ((shift) . 2) ((control)))
- ;; 速度を無視する
+ ;; ignore speed.
  mouse-wheel-progressive-speed nil)
 
 
@@ -21,6 +45,18 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; default background color
 (set-face-background 'default "#282828")
+
+;; line number settings.
+(global-linum-mode t)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; neotree settings.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'neotree)
+(global-set-key [f8] 'neotree-toggle)
+(setq-default neo-show-hidden-files t)
+(setq neo-theme (if (display-graphic-p) 'icons 'arrow))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -32,7 +68,7 @@
   (if delete-trailing-whitespece-before-save
       (delete-trailing-whitespace)))
 (add-hook 'before-save-hook 'my-delete-trailing-whitespace)
-; 無効にしたいモードのhook
+; hook to be disabled.
 (add-hook 'markdown-mode-hook
           '(lambda ()
              (set (make-local-variable 'delete-trailing-whitespece-before-save) nil)))
